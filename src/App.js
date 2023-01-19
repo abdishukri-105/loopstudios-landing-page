6ee1ba9f26d32b9eb6495e64ba3a6cdeaf167da0
abdishukri <abdishukrimohamed105@gmail.com>
2023-01-19 13:04:44 +0300

    done with navbar and landingpage ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 
 import './App.css';
-
+import NavBar from './NavBar';
+// import HeroImage from "../images/desktop/image-hero.jpg"
+import Hero from './Hero';
 function App() {
   return (
-    <div className="App bg-sky-600">
-      <h1 className="text-3xl font-bold underline bg-teal">
-        Hello world!
-    </h1>
-    <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-  <div class="shrink-0">
-    <img class="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo"/>
-  </div>
-  <div>
-    <div class="text-xl font-medium text-black">ChitChat</div>
-    <p class="text-slate-500">You have a new message!</p>
-  </div>
-</div>
 
-
+    <div className="App ">
+     
+     <div className="bg-[url('../images/desktop/image-hero.jpg')] h-screen ">
+      <div className='ml-14 mr-14'>
+      <NavBar />
+      </div>
+      <Hero />
+    </div>
 
     </div>
   );
---
+++ src/Hero.js
@@ -0,0 +1,9 @@
+const Hero = () => {
+    return ( 
+        <div className="flex  ml-48 items-center ">
+    <p className="text-4xl  text-white tracking-wide uppercase border border-white p-4 mt-20 pr-12">Immersive <br></br> experiences <br></br> that deliver</p>
+</div>
+     );
+}
+ 
+export default Hero;
